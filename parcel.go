32a4f5dc2aea373d944972f41f81ba2df9f53ccc
evgzor <>
2025-11-04 12:18:41 +0300

    -f

--- parcel.go
+++ parcel.go
@@ -58,7 +58,7 @@ func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
 	var res []Parcel
 	rows, err := s.db.Query("SELECT number, client, status, address, created_at FROM parcel WHERE client = :client", sql.Named("client", client))
 	if err != nil {
-		return res, err
+		return nil, err
 	}
 	defer rows.Close()
 
